3bccf923d74b5c618af82f5c7da7e533a994de3d
rahul raj <rahul.raj@quantuminventions.com>
2020-07-30 23:58:56 +0530

    Updates

---
+++ precache-manifest.338075e95a8f0e5d059e2d36eae3d833.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2bb38fa600372858a49aaea908fc87",
+    "url": "/shopLocal/index.html"
+  },
+  {
+    "revision": "6d5edb56d09576f61247",
+    "url": "/shopLocal/static/css/main.c6401d92.chunk.css"
+  },
+  {
+    "revision": "9c709b4dd77f0272502c",
+    "url": "/shopLocal/static/js/2.0df648b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopLocal/static/js/2.0df648b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5edb56d09576f61247",
+    "url": "/shopLocal/static/js/main.cbe99684.chunk.js"
+  },
+  {
+    "revision": "4176783ecca74b0e8c32",
+    "url": "/shopLocal/static/js/runtime-main.1744a016.js"
+  },
+  {
+    "revision": "6020641aeee769a82113cc4dc1192d01",
+    "url": "/shopLocal/static/media/orders.6020641a.png"
+  },
+  {
+    "revision": "09ba6fa685b692a9ac648212bfdbb29a",
+    "url": "/shopLocal/static/media/raj.09ba6fa6.jpeg"
+  },
+  {
+    "revision": "2aaa6f1be965eb98de80e55286525ff6",
+    "url": "/shopLocal/static/media/search.2aaa6f1b.png"
+  },
+  {
+    "revision": "5f8644f0a6144a865722b287ae396a2c",
+    "url": "/shopLocal/static/media/shopping-cart.5f8644f0.png"
+  },
+  {
+    "revision": "02723a8b181c646ad15095dd4786dac1",
+    "url": "/shopLocal/static/media/user.02723a8b.png"
+  }
+]);
